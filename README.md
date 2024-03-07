94d46364285a190f49c543731fa2569b099a2b6e
Arpita Dash <arpitadash1998@gmail.com>
2024-03-07 15:11:23 +0530

    adding readme and commenting print

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # codevisualization
 To visualize react code repo based on imports
+# Requirements 
+You will need python3 installed
+To install the necessary imports:
+pip3 install networkx
+pip3 install pyvis
+
+# Running the file
+1. Paste this file in the parent component where your web and native repos have been cloned into
+2. run the command 'python3 create_component.py'
+3. Answer the following questions:
+ 'What is the app you are interested in?' - the app name as stored in the code repo eg.: appName
+  'Interactions within which folder?' - which folder within the app you are interested in. eg.: components
+   'Any other interactions you want to visualize?(y/n)' - if you want to visualize the secondary interactions of the files in the previously selected folder with files from another folder
+    'What is the interaction folder name?' - the absolute path of the folder: eg.:src/widgets
+4. After the file is successfully run, a file called nx_appName.html will be created. Open that on chrome.
+
--- create_component.py
+++ create_component.py
@@ -1,14 +1,9 @@
 import os
-from anytree import Node, RenderTree
-from collections import deque
 import networkx as nx
 from random import randint
 from pyvis.network import Network
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
 import re
 import pyvis._version
-import networkx as nx
 
 root = './ekart-cl-ui'
 Savefolder = './'
@@ -30,50 +25,50 @@ def routeDict(fileName, importDict):
     # Example code snippet
     with open(fileName, 'r') as file:
         code = file.read()
-    # print('File: ',code)
+    # #print('File: ',code)
     # Define a regular expression pattern to match ProtectedRoute tags
     pattern = r'<\w*Route(?:.|\n)*?\n+?\s+?/>?\n+'
 
     # Find all matches of ProtectedRoute tags and extract the render attribute content
     matches = re.findall(pattern, code)
-    # print(list(importDict['import'].items()))
-    # print('Link!', importDict['link'])
+    # #print(list(importDict['import'].items()))
+    # #print('Link!', importDict['link'])
     # Initialize an empty dictionary to store component-link pairs
     component_link_dict = {}
-    # print(importDict['import'].items())
+    # #print(importDict['import'].items())
     # Iterate through the matches and extract component and link information
     for imp in importDict['import'].items():
         for match in matches:
-            # print('Each match', match)
+            # #print('Each match', match)
             # Extract the component and link using regular expressions
             if imp[1][0] in match:
-                # print(imp[1][0])
+                # #print(imp[1][0])
 
                 component_match = re.findall(r'render(?:.|\n)+?\/>', match)
                 link_match1 = re.findall(r"path?.*?\{?.*?\}\n+", match)
                 
-                print('Link? ', link_match1)
+                ##print('Link? ', link_match1)
                 
                 if component_match and link_match1:
                     link_match = link_match1[0].split("/")
-                    # print(link_match)
+                    # ##print(link_match)
                     if len(link_match)>1:
                         link = importDict['link']+'/'+'/'.join(('/'.join(link_match[1:])).split('`')[:-1])
                     else:
                         link = importDict['link']
-                    # print('Component? ', component_match, imp[0])
-                    # print('Path? ', link_match1)
+                    # #print('Component? ', component_match, imp[0])
+                    # #print('Path? ', link_match1)
                     component_link_dict[imp[0]] = link
                 break
             
     # Print the resulting dictionary
-    # print(component_link_dict)  
+    # #print(component_link_dict)  
     return(component_link_dict)  
 def createAbsFilePath(base,fileName):
     absFilePath1 = fileName.split('/')
     if absFilePath1[-1]=='index':
         absFilePath = absFilePath1[:-1]
-        # print(absFilePath)
+        # #print(absFilePath)
     else:
         absFilePath = absFilePath1
     if ("." or "..") in fileName:
@@ -120,12 +115,12 @@ def parseFileForImports(root,base,f, interestedFolder, interestedFolderB):
                     importComp = ''.join(''.join(fileLine_temp.split('import')[1].split('from')[0].split('{ ')).split(' }')).replace(" ","").split(',')
                 if 'index' in f and len(connectedFiles)==0 and ('export' in line and 'from' in line):
                     fileName = fileLine_temp.split('from')[-1].split(';')[0].strip().strip("'")
-                    # print(fileLine_temp.split('export')[1].split('from')[0].split('{ ')).split(' }')
+                    # #print(fileLine_temp.split('export')[1].split('from')[0].split('{ ')).split(' }')
                     importComp = ''.join(''.join(fileLine_temp.split('export')[1].split('from')[0].split('{ ')).split(' }')).replace(" ","").split(',')
                 finalRelPath = os.path.relpath(createAbsFilePath(base,fileName), root+interestedFolder)
                 finalRelPathB = os.path.relpath(createAbsFilePath(base,fileName), root+interestedFolderB)
                 if (".." not in finalRelPath or ".." not in finalRelPathB) and 'style' not in finalRelPath and 'Style' not in finalRelPath and 'Loader' not in finalRelPath:
-                    # print(finalRelPath)
+                    # #print(finalRelPath)
                     connectedFiles[finalRelPath] = importComp
             
     return connectedFiles
@@ -159,7 +154,7 @@ def parseFileForImports(root,base,f, interestedFolder, interestedFolderB):
     #                 importComp = ''.join(''.join(fileLine_temp.split('import')[1].split('from')[0].split('{ ')).split(' }')).replace(" ","").split(',')
     #             if 'index' in f and len(connectedFiles)==0 and ('export' in line and 'from' in line):
     #                 fileName = fileLine_temp.split('from')[-1].split(';')[0].strip().strip("'")
-    #                 # print(fileLine_temp.split('export')[1].split('from')[0].split('{ ')).split(' }')
+    #                 # #print(fileLine_temp.split('export')[1].split('from')[0].split('{ ')).split(' }')
     #                 importComp = ''.join(''.join(fileLine_temp.split('export')[1].split('from')[0].split('{ ')).split(' }')).replace(" ","").split(',')
     #             finalRelPath = os.path.relpath(createAbsFilePath(base,fileName), root+interestedFolder)
     #             finalRelPathB = os.path.relpath(createAbsFilePath(base,fileName), root+interestedFolderB)
@@ -173,7 +168,7 @@ def getMainRoute(root, appName, interestedFolder, interestedFolderB, JustAppName
     for base, dirs, files in os.walk(root+appName):
             for ind,f in enumerate(files):
                 if ('AuthWrapper' in f or 'RootComponent' in f) and (f.endswith(('.tsx')) or f.endswith(('.ts')) or f.endswith('.js')) :
-                    print(f)
+                    #print(f)
                     finalRelFilePath = os.path.relpath(base,  root+interestedFolder)
                     group = os.path.relpath(base,  root+appName)
                     fname = finalRelFilePath+'/'+f.split('.')[0]
@@ -193,7 +188,7 @@ def createDict(root, appName, colorDict, interestedFolder, interestedFolderB):
     appNameInd = appNameTemp.index('apps')
     JustAppName = appNameTemp[appNameInd+1]
     mainRoute = getMainRoute(root,appName, interestedFolder, interestedFolderB, JustAppName)
-    print('MAIN ROUTE',mainRoute)
+    #print('MAIN ROUTE',mainRoute)
     if flag>-1:
         for base, dirs, files in os.walk(root+appName):
             dirs.sort()
@@ -203,7 +198,7 @@ def createDict(root, appName, colorDict, interestedFolder, interestedFolderB):
                     group = os.path.relpath(base,  root+appName)
                     fname = finalRelFilePath+'/'+f.split('.')[0]
                     connectedFiles = parseFileForImports(root,base, f, interestedFolder, interestedFolderB)
-                    print('Next file', fname)
+                    #print('Next file', fname)
                     if 'index' in f:
                         fname=finalRelFilePath
                     if connectedFiles.__len__:
@@ -217,18 +212,18 @@ def createDict(root, appName, colorDict, interestedFolder, interestedFolderB):
                     # if('AuthWrapper' in f):
                     #     importDictFiles[fname]['link']='http://10.24.1.71/hub-ops'
                     if('Routes' in f or 'AuthWrapper' in f):
-                        # print(f)
+                        # #print(f)
                         if fname in mainRoute:
                             importDictFiles[fname]['link']=mainRoute[fname]
                         routeDict1=routeDict(base+'/'+f, importDictFiles[fname])
-                        # print('Current imports', routeDict1)
-                        # print(routeDict1.items())
+                        # #print('Current imports', routeDict1)
+                        # #print(routeDict1.items())
                         for k in routeDict1.items():
-                            print(k)
+                            #print(k)
                             if k[0] in importDictFiles.keys():
-                                print(k[0])
+                                #print(k[0])
                                 importDictFiles[k[0]]['link']=k[1]
-                                print(importDictFiles[k[0]])
+                                #print(importDictFiles[k[0]])
                             else:
                                 importDictFiles[k[0]]={'link': k[1]}
                     for k in colorDict.keys():
@@ -284,3 +279,4 @@ if __name__ == '__main__':
     colorDict = createColorDict(contentList, interestedFolder)
     [importDictFiles, dictNodes, nodeGroup, nodeColorDict] = createDict(root,interestedFolder, colorDict, interestedFolder, interestedFolderB)
     createGraph(dictNodes, importDictFiles, nodeGroup, './nx_'+appNameInp)
+    print('Please open the file now')
